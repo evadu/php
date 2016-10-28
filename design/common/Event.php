b9eac6eb7550f6213be78f79dda1873b238edae9
evadu <du_denglan@163.com>
2016-10-28 18:47:49 +0800

    观察者设计

---
+++ design/common/Event.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: dudenglan
+ * Date: 2016/10/28
+ * Time: 11:15
+ */
+
+namespace design\common;
+
+
+class Event
+{
+    CONST EVENT_PRICE_CHANGE = "event_price_change";
+}
